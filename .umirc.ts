f580c56f4bcd1c4c345a0703d09fcadc23906542
Notryag <1135814285@qq.com>
2021-12-21 23:45:23 +0800

    feat:add gh-pages

--- .umirc.ts
+++ .umirc.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  history:{
+    type:'hash'
+  },
   nodeModulesTransform: {
     type: 'none',
   },
+  base: './', // 打包路径，默认是/
+  publicPath: './', // 资源访问路径，默认/
   routes: [
     { path: '/', component: '@/pages/index' },
   ],
   fastRefresh: {},
-  alias: {
-    '@': './src',
-  }
 });
